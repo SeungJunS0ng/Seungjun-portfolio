a3c14f642a57cd24acd4cf3e824f90fa1f860774
송승준 <songseungjun@songseungjun-ui-iMac.local>
2026-09-01 12:47:02 +0900

    feat: MyPage 사용자 정보 표시 기능 추가
    - MyPage.tsx: Redux에서 사용자 정보 조회
    - 로그인한 사용자의 이메일 표시

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,12 @@
+import { useAppSelector } from '../app/hooks'
+
 export default function MyPage() {
-  return (
-    <div>
-      <h1>마이페이지</h1>
-      <p>프로필 및 설정</p>
-    </div>
-  )
+    const user = useAppSelector(state => state.auth.user)
+
+    return (
+        <>
+            <h1>My Page</h1>
+            <p>안녕하세요, {user?.email} 님</p>
+        </>
+    )
 }
